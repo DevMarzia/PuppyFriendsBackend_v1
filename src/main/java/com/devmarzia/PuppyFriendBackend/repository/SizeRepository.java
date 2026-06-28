c32ee67a2ec04d71bb35035e816a21a56989ddc7
Marzia Bonsegna <126612092+DevMarzia@users.noreply.github.com>
2026-06-28 15:03:38 +0200

    Update SizeRepository.java

--- src/main/java/com/devmarzia/PuppyFriendBackend/repository/SizeRepository.java
+++ src/main/java/com/devmarzia/PuppyFriendBackend/repository/SizeRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-// <Size, Long> equivale a dire che: "Gestisco entità di tipo Size che hanno un ID di tipo Long"
-
 public interface SizeRepository extends JpaRepository<Size, Long> {
     Optional<Size> findByLabel(String label);
 }
